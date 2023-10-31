2d2ec8a7facc2c001e469847215d7b1d612db0f8
SHWestbrook <pedrosanchezromero213@gmail.com>
2023-10-31 17:10:53 +0100

    cambios

--- registro.js
+++ registro.js
@@ -2,12 +2,19 @@ function validarFormulario(){
     var email = document.getElementById("email");
     var emailRegex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ;
 
+    var dni= document.getElementById("dni");
+    var dniRegex= /^\d{8}[A-HJ-NP-TV-Z]$/i;
+
     var contra = document.getElementById("passwd");
     var confirContra = document.getElementById("cpasswd");
 
     if(!emailRegex.test(email.value)){
         alert("El email no es valido");
         return false;
+    }
+
+    if(!dniRegex.test(dni.value)){
+        alert("El dni no es valido introduce otro")
     }
 
     if(contra !== confirContra){
